ba06e32d7695925ab31cf43f10275f699909b093
a.vasiliev <a.vasiliev@logistictech.ru>
2018-03-12 18:19:57 +0300

    Create Tag model

---
+++ visual_novel/cinfo/tags/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+from core.models import PublishModel
+
+
+class Tag(PublishModel):
+    title = models.CharField(verbose_name='название', max_length=256)
+    description = models.TextField(verbose_name='описание', max_length=5000, default='')
+
+    class Meta:
+        db_table = 'tags'
+        verbose_name = 'Тэг'
+        verbose_name_plural = 'Тэги'
+
+    def __str__(self):
+        return self.title
