de6cadb8163bc14446715f526eb475d5874987b8
a.vasiliev <a.vasiliev@logistictech.ru>
2018-03-12 18:21:44 +0300

    Create StaffRole model

---
+++ visual_novel/cinfo/staff_roles/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+from core.models import PublishModel
+
+
+class StaffRole(PublishModel):
+    title = models.CharField(verbose_name='название', max_length=256)
+
+    class Meta:
+        db_table = 'staff_roles'
+        verbose_name = 'Должность'
+        verbose_name_plural = 'Должности'
+
+    def __str__(self):
+        return self.title
