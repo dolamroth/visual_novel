baea8cef4d7a73bea11edef884099db473993336
SergeySayamov <s.sayamov@logistictech.ru>
2018-03-13 19:07:19 +0300

    Customize admin for visual novel

--- visual_novel/vn_core/admin.py
+++ visual_novel/vn_core/admin.py
@@ -1,5 +1,34 @@
 from django.contrib import admin
 
-from .models import VisualNovel
+from .models import VisualNovel, VNGenre, VNStaff, VNStudio, VNTag
 
-admin.site.register(VisualNovel)
+
+class GenreInline(admin.TabularInline):
+    model = VNGenre
+    extra = 3
+
+
+class TagInline(admin.TabularInline):
+    model = VNTag
+    extra = 3
+
+
+class StudioInline(admin.TabularInline):
+    model = VNStudio
+    extra = 3
+
+
+class StaffInline(admin.TabularInline):
+    model = VNStaff
+    extra = 3
+
+
+class VisualNovelAdmin(admin.ModelAdmin):
+    inlines = (GenreInline, TagInline, StudioInline, StaffInline, )
+    list_display = (
+        'title', 'alternative_title', 'photo', 'description', 'date_of_release', 'longevity',
+        'vndb_id', 'steam_link', 'alias'
+    )
+
+
+admin.site.register(VisualNovel, VisualNovelAdmin)
