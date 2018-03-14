bbf266423a40a4fb5d513d198d18a25276bb50e2
a.vasiliev <a.vasiliev@logistictech.ru>
2018-03-14 20:49:44 +0300

    Add tags to migration

--- visual_novel/cinfo/migrations/0009_data_migration_genre_studio_staff_tag.py
+++ visual_novel/cinfo/migrations/0009_data_migration_genre_studio_staff_tag.py
@@ -30,6 +30,157 @@ def add_genres(apps, schema_editor):
     war, _ = Genre.objects.get_or_create(title='война', alias='war')
 
 
+def add_tags(apps, schema_editor):
+    Tag = apps.get_model('cinfo', 'Tag')
+    nakige, _ = Tag.objects.get_or_create(
+        title='накиге',
+        description="""Накиге (от 泣きゲ, буквально "плачущая игра")&nbsp;&#8211; подвид визуальных новелл, 
+                        основанный на идее заставить игрока сопереживать героям, испытать сильные душевные переживания. 
+                        Такие игры следуют традиционной формуле: первая комедийная часть игры раскрывает персонажей, 
+                        в то время как в основе второй лежат глубоко эмоциональные, пробивающие на слезу истории.""",
+        alias='nakige'
+    )
+    storige, _ = Tag.objects.get_or_create(
+        title='сториге',
+        description="""Сториге (от англ. "story"&nbsp;&#8211; "история" и ゲ, сокр. от "ゲーム"&nbsp;&#8211; "игра")
+                        &nbsp;&#8211; подвид визуальных новелл, в которых основной упор делается на сюжет.""",
+        alias='storige'
+    )
+    eroge, _ = Tag.objects.get_or_create(
+        title='эроге (18+)',
+        description="""Эроге (エロゲ, от англ. "Erotic game")&nbsp;&#8211; японская порнографическая игра. От обычной 
+                        визуальной новеллы отличается наличием в ней 
+                        порнографических материалов, не предназначенных для несовершеннолетних.""",
+        alias='eroge'
+    )
+    all_18, _ = Tag.objects.get_or_create(
+        title='(18+/all)',
+        alias='all_18'
+    )
+    all_age, _ = Tag.objects.get_or_create(
+        title='для всех возрастов (all)',
+        alias='all-age'
+    )
+    chunige, _ = Tag.objects.get_or_create(
+        title='чуниге',
+        description="""Чуниге (от "中二病" ("синдром восьмиклассника") и "ゲーム" ("игра"))&nbsp;&#8211; подвид 
+                        визуальных новелл, в которых одними из основополагающих образующих сюжета являются пафосные 
+                        бои, геройская тематика, превозмогания, магия и суперспособности.""",
+        alias='chunige'
+    )
+    true_end, _ = Tag.objects.get_or_create(
+        title='есть истинная концовка',
+        alias='true-end'
+    )
+    adv, _ = Tag.objects.get_or_create(
+        title='ADV',
+        description="""ADV&nbsp;&#8211; вид визуальных новелл, в котором текстовая часть располагается в небольшом 
+                        окошке в нижней части экрана. Самый распространённый вид визуальных новелл.""",
+        alias='ADV'
+    )
+    cinetic_novell, _ = Tag.objects.get_or_create(
+        title='кинетическая новелла',
+        description="""Вид визуальных новелл, в которых полностью отсутствуют ветвления и влияние читателя на сюжет.""",
+        alias='kinetic-novel'
+    )
+    NVL, _ = Tag.objects.get_or_create(
+        title='NVL',
+        description='Обозначение для новелл, в которых текстовая панель занимает большую часть экрана.',
+        alias='NVL'
+    )
+    ucuge, _ = Tag.objects.get_or_create(
+        title='уцуге',
+        description="""Уцуге (яп. 鬱ゲー) дословно означает "депрессивная игра". Как и следует из названия, 
+                        целью таких игр является ввести человека в угнетённое, подавленное состояние. Для их сюжетов 
+                        характерно наличие различных безвыходных и тяжёлых ситуаций без какой-либо надежды на улучшение.
+                         В отличие от накиге, где зачастую присутствуют "хорошие" концовки, в уцуге история обречена 
+                         на несчастливый финал.""",
+        alias='ucuge'
+    )
+    guro, _ = Tag.objects.get_or_create(
+        title='гуро',
+        description="""Гуро (Erotic Grotesque, Guro)&nbsp;&#8211; характеризуется наличием сцен, вызывающих отвращение 
+                        у большинства людей,&nbsp;&#8211; странных, абсурдных и выходящих за рамки привычного. 
+                        Обычно это эротические сцены с расчленёнными или выпотрошенными телами, кровопролитие, 
+                        каннибализм, отрезание конечностей, извращённые убийства, некрофилия, пластические операции.""",
+        alias='guro'
+    )
+    madness, _ = Tag.objects.get_or_create(
+        title='безумие',
+        alias='madness'
+    )
+    magic, _ = Tag.objects.get_or_create(
+        title='магия',
+        alias='magic'
+    )
+    despair, _ = Tag.objects.get_or_create(
+        title='отчаяние',
+        alias='despair'
+    )
+    moege, _ = Tag.objects.get_or_create(
+        title='моэге',
+        description="""Моэге (萌えゲー, moege)&nbsp;&#8211; визуальные новеллы или эроге с большим содержанием 
+                        <a href="https://ru.wikipedia.org/wiki/%D0%9C%D0%BE%D1%8D">моэ</a>-контента. Для игр данного 
+                        жанра характерны простые и "лёгкие" сюжеты, приятное визуальное и звуковое сопровождение, а 
+                        также большое наличие стереотипных персонажей.""",
+        alias='moege'
+    )
+    kiarage, _ = Tag.objects.get_or_create(
+        title='кяраге',
+        decription="""От "character" и "game". Вид визуальных новелл, в которых акцент делается на взаимоотношениях 
+                        персонажей. Часто пересекается с моэге.""",
+        alias='kiarage'
+    )
+    stable_game_order, _ = Tag.objects.get_or_create(
+        title='неизменяемый порядок прохождения',
+        description="""Определённые руты (сюжетные ветки) игры открываются только после прочтения других.""",
+        alias='stable-game-order'
+    )
+    voice_novel, _ = Tag.objects.get_or_create(
+        title='звуковая новелла',
+        description="""Та же кинетическая новелла, но с большим упором на звуковое сопровождение. Зачастую 
+                        представители жанра имеют весьма слабую графическую составляющую, 
+                        либо не имеют таковой вовсе.""",
+        alias='voice-novel'
+    )
+    denpa, _ = Tag.objects.get_or_create(
+        title='денпа',
+        description="""В таких новеллах герои, которые до поры вели себя нормально, вдруг начинают вести себя 
+                        неестественным образом, словно на них напало помешательство. Главный герой не всегда 
+                        является исключением.""",
+        alias='denpa'
+    )
+    philosophy, _ = Tag.objects.get_or_create(
+        title='философия',
+        alias='philosophy'
+    )
+    linear_plot, _ = Tag.objects.get_or_create(
+        title='линейный сюжет',
+        description="""Игра содержит целиком линейный сюжет, и привычные для визуальных новелл руты (сюжетные арки) 
+                        отсутствуют. Это не значит, что в новелле вообще не может быть выборов, но, если есть, они не 
+                        оказывают большого воздействия на сюжет.""",
+        alias='linear-plot'
+    )
+    steampunk, _ = Tag.objects.get_or_create(
+        title='стимпанк',
+        alias='steampunk'
+    )
+    cyberpunk, _ = Tag.objects.get_or_create(
+        title='киберпанк',
+        alias='cyberpunk'
+    )
+    SRPG, _ = Tag.objects.get_or_create(
+        title='SRPG',
+        description="""Strategy Role-playing game""",
+        alias='SRPG'
+    )
+    nukige, _ = Tag.objects.get_or_create(
+        title='нукиге',
+        description="""Подвид визуальных новелл, в которых основной упор делается на хентайную составляющую.""",
+        alias='nukige'
+    )
+
+
 class Migration(migrations.Migration):
 
     dependencies = [
@@ -37,5 +188,5 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
-        migrations.RunPython(add_genres),
+        migrations.RunPython(add_genres, add_tags),
     ]
