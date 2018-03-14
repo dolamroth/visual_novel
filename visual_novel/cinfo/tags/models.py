10dba25283dddc1d35eb4a1c986e0350406cf314
a.vasiliev <a.vasiliev@logistictech.ru>
2018-03-14 17:54:09 +0300

    добавление alias к моделям в cinfo

--- visual_novel/cinfo/tags/models.py
+++ visual_novel/cinfo/tags/models.py
@@ -5,6 +5,7 @@ from core.models import PublishModel
 class Tag(PublishModel):
     title = models.CharField(verbose_name='название', max_length=256)
     description = models.TextField(verbose_name='описание', max_length=5000, default='')
+    alias = models.TextField(verbose_name='алиас (до 30 символов)', max_length=30, default='')
 
     class Meta:
         db_table = 'tags'
