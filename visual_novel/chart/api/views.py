2c14f22eae83273e63e97e3af0af7daaed83e39a
a.vasiliev <a.vasiliev@logistictech.ru>
2018-03-16 23:34:16 +0300

    Add serializer for VisualNovel, fix chart/item.html pattern.

--- visual_novel/chart/api/views.py
+++ visual_novel/chart/api/views.py
@@ -3,17 +3,22 @@ from rest_framework.views import APIView
 from rest_framework.renderers import TemplateHTMLRenderer
 from rest_framework.exceptions import NotFound
 
+
 from chart.models import ChartItem
+
+from .serializers import VisualNovelSerializer
 
 
 class ChartView(APIView):
-    renderer_classes = [TemplateHTMLRenderer]
-    template_name = 'chart/item.html'
+    #renderer_classes = [TemplateHTMLRenderer]
+    #template_name = 'chart/item.html'
 
     def get(self, request, vn_alias):
         try:
             chart_item = ChartItem.objects.get(visual_novel__alias=vn_alias)
         except ChartItem.DoesNotExist:
             raise NotFound
-        return Response(status=200)
+        novel = chart_item.visual_novel
+        serializer = VisualNovelSerializer(novel)
+        return Response(data=serializer.data, status=200)
 
