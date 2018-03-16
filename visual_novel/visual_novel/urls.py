bd5ec72cfd5a0131f47bda6bcef3a224eb499d1b
SergeySayamov <s.sayamov@logistictech.ru>
2018-03-16 23:44:02 +0300

    Base chart index view

--- visual_novel/visual_novel/urls.py
+++ visual_novel/visual_novel/urls.py
@@ -4,7 +4,10 @@ from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
 
+from chart import views as chart_views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', TemplateView.as_view(template_name="pages/index.html"), name='main')
+    path('', TemplateView.as_view(template_name="pages/index.html"), name='main'),
+    path('chart/', chart_views.chart_index_page, name='chart_main')
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
