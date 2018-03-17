ab5a88692332441fd17da1d0374dca94599e1d04
SergeySayamov <s.sayamov@logistictech.ru>
2018-03-17 21:44:32 +0300

    Fixes in chart item page

--- visual_novel/chart/views.py
+++ visual_novel/chart/views.py
@@ -1,6 +1,6 @@
 import os
 
-from django.shortcuts import render, render_to_response
+from django.shortcuts import render
 from django.conf import settings
 
 from .models import ChartItem
@@ -68,59 +68,69 @@ def chart_page(request, vn_alias):
     vn_context['description'] = visual_novel.description
     vn_context['vndb_id'] = visual_novel.vndb_id
     vn_context['chart_link'] = os.path.join('/chart/', visual_novel.alias)
-    vn_context['steam_link'] = visual_novel.steam_link
+    vn_context['has_steam'] = not not visual_novel.steam_link
+    vn_context['steam_link'] = visual_novel.steam_link or ''
+    vn_context['steam_icon'] = settings.STEAM_ICON_URL
     vn_context['longevity'] = visual_novel.longevity.__str__()
+    vn_context['longevity_link'] = '/chart/' # TODO: fix
     vn_context['date_of_release'] = visual_novel.date_of_release
     vn_context['date_of_translation'] = chart_item.date_of_translation
+    vn_context['vndb_mark'] = 0 # TODO: fix
 
     vn_context['genres'] = list()
-    for genre in visual_novel.vngenre_set.all().order_by('-weight'):
+    for genre in visual_novel.vngenre_set.filter(genre__is_published=True).order_by('-weight'):
         vn_context['genres'].append({
             'title': genre.genre.title,
             'description': genre.genre.description,
             'link': os.path.join('/chart/', 'genre', genre.genre.alias)
         })
+    vn_context['has_genres'] = (len(vn_context['genres']) > 0)
 
     vn_context['studios'] = list()
-    for studio in visual_novel.vnstudio_set.all().order_by('-weight'):
+    for studio in visual_novel.vnstudio_set.filter(studio__is_published=True).order_by('-weight'):
         vn_context['studios'].append({
             'title': studio.studio.title,
             'description': studio.studio.description,
             'link': os.path.join('/chart/', 'studio', studio.studio.alias)
         })
+    vn_context['has_studios'] = (len(vn_context['studios']) > 0)
 
     vn_context['tags'] = list()
-    for tag in visual_novel.vntag_set.all().order_by('-weight'):
+    for tag in visual_novel.vntag_set.filter(tag__is_published=True).order_by('-weight'):
         vn_context['tags'].append({
             'title': tag.tag.title,
             'description': tag.tag.description,
             'link': os.path.join('/chart/', 'tag', tag.tag.alias)
         })
+    vn_context['has_tags'] = (len(vn_context['tags']) > 0)
 
     vn_context['staffs'] = list()
     staffs_clear = list()
-    for staff in visual_novel.vnstaff_set.all().order_by('-weight'):
+    for staff in visual_novel.vnstaff_set.filter(staff__is_published=True).order_by('-weight'):
         if staff.staff not in staffs_clear:
             staffs_clear.append(staff.staff)
     for staff in staffs_clear:
-        vnstaffs = VNStaff.objects.filter(staff=staff).distinct('role__title')
+        vnstaffs = VNStaff.objects.filter(staff=staff, visual_novel=visual_novel).distinct('role__title')
         roles = [vnstaffs[i].role for i in range(len(vnstaffs))]
         vn_context['staffs'].append({
             'title': staff.title,
             'link': os.path.join('/chart/', 'staff', staff.alias),
             'roles': roles
         })
+    vn_context['has_staffs'] = (len(vn_context['staffs']) > 0)
 
     vn_context['screenshots'] = list()
-    vn_screenshots = chart_item.chartitemscreenshot_set.all().order_by('-order')
-    if len(vn_screenshots) == 0:
-        vn_context['screen'] = 'test'
+    vn_screenshots = chart_item.chartitemscreenshot_set.filter(
+        is_published=True,
+        image__isnull=False,
+        miniature__isnull=False
+    ).order_by('-order')
     for screenshot in vn_screenshots:
         vn_context['screenshots'].append(
             {
                 'title': screenshot.title,
-                'image': screenshot.image.url if screenshot.image else settings.POSTER_STOPPER_URL,
-                'miniature': screenshot.miniature.url if screenshot.miniature else settings.POSTER_STOPPER_URL
+                'image': screenshot.image.url,
+                'miniature': screenshot.miniature.url
             }
         )
 
--- visual_novel/static/js/custom.js
+++ visual_novel/static/js/custom.js
@@ -1,4 +1,22 @@
+$('.vn-desc-unwrap').click(function(){
+	$(this).toggleClass('hidden-option').next().toggleClass('hidden-option').next().toggleClass('hidden-option');
+	return false;
+});
 
-$('.tip').tooltip();
-<style>
-</style>
+$('.vn-desc-wrap').click(function(){
+	$(this).toggleClass('hidden-option').prev().toggleClass('hidden-option').prev().toggleClass('hidden-option');
+	return false;
+});
+
+$(function () {
+	$('.popup-modal').magnificPopup({
+		type: 'inline',
+		preloader: false,
+		focus: '#username',
+		modal: true
+	});
+	$(document).on('click', '.popup-modal-dismiss', function (e) {
+		e.preventDefault();
+		$.magnificPopup.close();
+	});
+});
