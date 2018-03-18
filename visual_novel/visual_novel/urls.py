fd205f592586be947628818313283891f9e65d5f
a.vasiliev <a.vasiliev@logistictech.ru>
2018-03-18 23:45:04 +0300

    Add offers in main urls

--- visual_novel/visual_novel/urls.py
+++ visual_novel/visual_novel/urls.py
@@ -5,9 +5,11 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 import chart.api.urls
+import offer_service.urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', TemplateView.as_view(template_name="pages/index.html"), name='main'),
-    path('chart/', include(chart.api.urls))
+    path('chart/', include(chart.api.urls)),
+    path('offers/', include(offer_service.urls), name='offers')
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
