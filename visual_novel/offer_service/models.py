69ed3afeca0acc0586e04841cf4b7eb39dbbd5ce
a.vasiliev <a.vasiliev@logistictech.ru>
2018-03-22 22:19:32 +0300

    Email sending when admin check offer status

--- visual_novel/offer_service/models.py
+++ visual_novel/offer_service/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from core.models import PublishModel
 from django.core.exceptions import ValidationError
+from notifications.service import send_email
 
 
 class Offer(PublishModel):
@@ -21,8 +22,29 @@ class Offer(PublishModel):
     def __str__(self):
         return 'Заявка от %s %s' % (self.email, self.timestamp.strftime("%d/%m/%y %H:%M"))
 
+    def __init__(self, *args, **kwargs):
+        super(Offer, self).__init__(*args, **kwargs)
+        self.__important_fields = ['is_considered', 'is_accepted', 'is_rejected']
+        for field in self.__important_fields:
+            setattr(self, '__original_%s' % field, getattr(self, field))
+
+    def has_changed(self):
+        for field in self.__important_fields:
+            orig = '__original_%s' % field
+            if getattr(self, orig) != getattr(self, field):
+                return True, field
+        return False, 'test'
+
     def save(self, *args, **kwargs):
         if self.is_accepted or self.is_rejected:
             self.is_published = False
         super(Offer, self).save(*args, **kwargs)
-
+        flag, type_email = self.has_changed()
+        # TODO more complex and informative text
+        # TODO add celery apply_async for speed 
+        if flag and type_email == 'is_considered':
+            send_email('Заявка', 'Ваша заявка принята к рассмотрению.', self.email)
+        if flag and type_email == 'is_accepted':
+            send_email('Заявка', 'Ваша заявка принята администрацией.', self.email)
+        if flag and type_email == 'is_rejected':
+            send_email('Заявка', 'Ваша заявка отклонена, нам жаль (нет).', self.email)
