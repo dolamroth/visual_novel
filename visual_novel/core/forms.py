95395b0df2e0c0b0c4be2e298a79f112ec1b9fcb
SergeySayamov <s.sayamov@logistictech.ru>
2018-03-24 17:03:08 +0300

    Reset password

--- visual_novel/core/forms.py
+++ visual_novel/core/forms.py
@@ -1,9 +1,13 @@
-from timezone_field import TimeZoneFormField
-
 from django import forms
-from django.contrib.auth.forms import UsernameField, AuthenticationForm, UserCreationForm
+from django.template import loader
+from django.contrib.auth.forms import (
+    UsernameField, AuthenticationForm, UserCreationForm, PasswordResetForm,
+    SetPasswordForm
+)
 from django.contrib.auth.models import User
 from django.utils.translation import gettext_lazy as _
+
+from notifications.service import send_email
 
 
 class CustomAuthentificationForm(AuthenticationForm):
@@ -14,7 +18,6 @@ class CustomAuthentificationForm(AuthenticationForm):
     )
 
     password = forms.CharField(
-        label=_("Password"),
         strip=False,
         widget=forms.PasswordInput(attrs={'class': 'form-control'}),
     )
@@ -29,10 +32,6 @@ class CustomAuthentificationForm(AuthenticationForm):
 
 
 class CustomSignUpForm(UserCreationForm):
-
-    error_messages = {
-        'password_mismatch': _("Введённые пароли не совпадают."),
-    }
 
     timezone = forms.IntegerField(
         widget=forms.TextInput(attrs={'class': 'form-control', 'type': 'hidden'}),
@@ -49,13 +48,11 @@ class CustomSignUpForm(UserCreationForm):
     )
 
     password1 = forms.CharField(
-        label=_("Пароль"),
         strip=False,
         widget=forms.PasswordInput(attrs={'class': 'form-control'}),
     )
 
     password2 = forms.CharField(
-        label=_("Подтверждение пароля"),
         widget=forms.PasswordInput(attrs={'class': 'form-control'}),
         strip=False,
     )
@@ -64,3 +61,34 @@ class CustomSignUpForm(UserCreationForm):
         model = User
         field_classes = {'username': UsernameField}
         fields = ('username', 'timezone', 'email', 'password1', 'password2',)
+
+
+class CustomPasswordResetForm(PasswordResetForm):
+
+    email = forms.EmailField(
+        max_length=254,
+        widget=forms.TextInput(attrs={'class': 'form-control', 'type': 'email'}),
+    )
+
+    def send_mail(self, subject_template_name, email_template_name,
+                  context, from_email, to_email, html_email_template_name=None):
+
+        subject = loader.render_to_string(subject_template_name, context)
+        # Email subject *must not* contain newlines
+        subject = ''.join(subject.splitlines())
+        message = loader.render_to_string(email_template_name, context)
+
+        send_email(subject, message, to_email)
+
+
+class CustomSetPasswordForm(SetPasswordForm):
+
+    new_password1 = forms.CharField(
+        widget=forms.PasswordInput(attrs={'class': 'form-control'}),
+        strip=False,
+    )
+
+    new_password2 = forms.CharField(
+        strip=False,
+        widget=forms.PasswordInput(attrs={'class': 'form-control'}),
+    )
--- visual_novel/visual_novel/urls.py
+++ visual_novel/visual_novel/urls.py
@@ -7,26 +7,53 @@ from django.urls import path, include
 from django.contrib.auth import views as auth_views
 from core import views as core_views
 
-from core.forms import CustomAuthentificationForm
+from core.forms import (
+    CustomAuthentificationForm, CustomPasswordResetForm, CustomSetPasswordForm
+)
 
 import offer_service.urls
 import chart.urls
 
 urlpatterns = [
+    # Admin panel views
     path('admin/', admin.site.urls),
-    path('login/', auth_views.login, {
-        'template_name': 'pages/login.html',
-        'redirect_authenticated_user': True,
-        'authentication_form': CustomAuthentificationForm,
-    }, name='login'),
-    path('logout/', auth_views.logout, {
-        'template_name': 'pages/logout.html',
-    }, name='logout'),
+
+    # Authentification views
+    path('login/', auth_views.LoginView.as_view(
+        template_name='pages/login.html',
+        form_class=CustomAuthentificationForm,
+        redirect_authenticated_user=True,
+    ), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(
+        template_name='pages/logout.html'
+    ), name='logout'),
     path('activate/<str:uidb64>/<str:token>/',
         core_views.activate, name='activate'),
     path('account_activation_sent/', core_views.account_activation_sent, name='account_activation_sent'),
     path('signup/', core_views.signup, name='signup'),
+
+    # Password reset views
+    path('password_reset/', auth_views.PasswordResetView.as_view(
+        email_template_name='pages/password_reset_email.html',
+        form_class=CustomPasswordResetForm,
+        template_name='pages/password_reset_form.html',
+        subject_template_name = 'pages/password_reset_subject.txt'
+    ), name='password_reset'),
+    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(
+        template_name = 'pages/password_reset_done.html'
+    ), name='password_reset_done'),
+    path('reset/<str:uidb64>/<str:token>/', auth_views.PasswordResetConfirmView.as_view(
+        template_name = 'pages/password_reset_confirm.html',
+        form_class = CustomSetPasswordForm
+    ), name='password_reset_confirm'),
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(
+        template_name = 'pages/password_reset_complete.html',
+    ), name='password_reset_complete'),
+
+    # Plain pages views
     path('', TemplateView.as_view(template_name="pages/index.html"), name='main'),
+
+    # Apps views
     path('offers/', include(offer_service.urls), name='offers'),
     path('chart/', include(chart.urls))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
