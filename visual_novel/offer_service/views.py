e93afeb1f454b82d1eae024bafc4f1369cd7c6ba
SergeySayamov <s.sayamov@logistictech.ru>
2018-03-25 12:11:55 +0300

    Visual fixes for offers template

--- visual_novel/offer_service/views.py
+++ visual_novel/offer_service/views.py
@@ -2,12 +2,19 @@ from django.shortcuts import render, HttpResponseRedirect
 from snowpenguin.django.recaptcha2.fields import ReCaptchaField
 from snowpenguin.django.recaptcha2.widgets import ReCaptchaWidget
 from django.forms import forms
-from django.urls import reverse
+from django.utils.translation import gettext_lazy as _
+
 from .models import Offer
 
 
 class CaptchaForm(forms.Form):
-    captcha = ReCaptchaField(widget=ReCaptchaWidget(), label='Проверка:')
+    captcha = ReCaptchaField(
+        widget=ReCaptchaWidget(),
+        label='Проверка',
+        error_messages={
+            'required': _('Обязательное поле.'),
+        }
+    )
 
 
 def send_offer(request, *args, **kwargs):
@@ -18,9 +25,9 @@ def send_offer(request, *args, **kwargs):
         form = CaptchaForm(request.POST)
         if form.is_valid():
             offer_text = request.POST['offer_text']
-            email = request.POST['email']
+            email = request.user.email
             offer = Offer.objects.create(email=email, offer=offer_text)
-            return HttpResponseRedirect('/')    # redirect if success
+            return HttpResponseRedirect('/') # redirect if success
         else:
-            return HttpResponseRedirect(reverse('send_offer'))  # redirect if fail
+            return render(request, 'support/offer.html', {'form': form, **kwargs}) # redirect if fail
 
