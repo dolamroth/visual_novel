981a75116aff8a4805fc8b3603dbbe006f754429
SergeySayamov <s.sayamov@logistictech.ru>
2018-03-28 21:21:42 +0300

    Admin panel in russian

--- visual_novel/visual_novel/settings/base.py
+++ visual_novel/visual_novel/settings/base.py
@@ -153,7 +153,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru-RU'
 
 TIME_ZONE = 'UTC'
 
