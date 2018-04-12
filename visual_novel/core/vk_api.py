11486b91058d25d95507fe502d4243582293b72f
SergeySayamov <s.sayamov@logistictech.ru>
2018-04-12 19:52:57 +0300

    Bugfix: determine user_id by alias

--- visual_novel/core/vk_api.py
+++ visual_novel/core/vk_api.py
@@ -2,32 +2,48 @@ import vk_api
 from django.conf import settings
 
 
+class VkError(Exception):
+    message = ''
+    def __str__(self):
+        assert self.message is not None, ('Неизвестная ошибка.')
+        return self.message
+
+
+class VkAuthError(VkError):
+    message = 'Неверный логин или пароль.'
+
+
+class VkGetUserError(VkError):
+    message = 'Такого пользователя не существует.'
+
+
 def vk_auth():
     login, password = settings.VK_LOGIN, settings.VK_PASSWORD
     vk_session = vk_api.VkApi(login, password)
     try:
         vk_session.auth()
-    except vk_api.AuthError as error_msg:
-        print(error_msg)  # TODO to log?
-        return None
+    except vk_api.AuthError:
+        raise VkAuthError()
     vk = vk_session.get_api()
     return vk
 
 
 def post_to_group_wall(message):
     vk = vk_auth()
-    if vk is None:
-        return
     response = vk.wall.post(owner_id=settings.VK_GROUP_ID, from_group=1, message=message)
     return response
 
 
 def post_to_user(user_id, message):
     vk = vk_auth()
-    if vk is None:
-        return
-    response = vk.messages.send(user_id=user_id, message=message)
+    response = vk.messages.send(user_id=get_user_id_by_alias(user_id), message=message)
     return response
 
 
-
+def get_user_id_by_alias(user_alias):
+    vk = vk_auth()
+    try:
+        response = vk.users.get(user_ids=user_alias)
+    except vk_api.exceptions.ApiError:
+        raise VkGetUserError()
+    return response[0]['id']
