d4062acee36f7acc3d4b1d349d3f18cf08868933
SergeySayamov <s.sayamov@logistictech.ru>
2018-04-26 19:21:16 +0300

    Create confirmed profile for staff

--- visual_novel/core/models.py
+++ visual_novel/core/models.py
@@ -57,5 +57,8 @@ class Profile(models.Model):
 @receiver(post_save, sender=User)
 def create_user_profile(sender, instance, created, **kwargs):
     if created:
-        Profile.objects.create(user=instance)
+        Profile.objects.create(
+            user=instance,
+            email_confirmed=instance.is_staff
+        )
     instance.profile.save()
