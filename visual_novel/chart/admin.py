f085600918a76db77abd323dad313d5ca39fdd75
a.vasiliev <a.vasiliev@logistictech.ru>
2018-05-07 17:55:23 +0300

    Transfer image_tag to admin from chart.models

--- visual_novel/chart/admin.py
+++ visual_novel/chart/admin.py
@@ -1,13 +1,23 @@
 from django.contrib import admin
+from django.utils.html import format_html
 
 from .models import ChartItem, ChartItemScreenshot
 
 
 class ScreenshotInline(admin.TabularInline):
+    def image_tag(self):
+        return format_html('<img src="%s" width=200 height=150 />' % self.image.url if self.image else '')
+
+    image_tag.short_description = 'Фотография'
+    image_tag.allow_tags = True
     model = ChartItemScreenshot
     fields = ('is_published', 'title', 'image', 'image_tag', 'order')
     readonly_fields = ('image_tag',)
     extra = 3
+
+    def get_queryset(self, request):
+        qs = super(ScreenshotInline, self).get_queryset(request)
+        return qs.exclude(image__exact='')
 
 
 class ChartItemAdmin(admin.ModelAdmin):
