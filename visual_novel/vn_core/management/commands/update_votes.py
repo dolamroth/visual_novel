c15f2a55fe4b8a6c71a615bd896d7c1f36ae04f1
a.vasiliev <a.vasiliev@logistictech.ru>
2018-05-27 15:34:11 +0300

    Transfer managment command to vn_core directory

--- visual_novel/vn_core/management/commands/update_votes.py
+++ visual_novel/vn_core/management/commands/update_votes.py
@@ -1,6 +1,8 @@
+import datetime
+
 from django.core.management.base import BaseCommand
 
-from vn_core.models import VisualNovel
+from vn_core.models import VisualNovel, VisualNovelStats
 from vn_core.utils import vndb_socket_login, vndb_socket_logout, vndb_socket_update_vn
 
 
@@ -9,18 +11,26 @@ class Command(BaseCommand):
 
         sock = vndb_socket_login()
         if sock is None:
+            print('Vndb connection error')
             return
 
         try:
-            all_visual_novels = VisualNovel.objects.filter(is_published=True).values_list('vndb_id', flat=True)
+            all_visual_novels = VisualNovel.objects.all().values_list('vndb_id', flat=True)
+            now_date = datetime.datetime.utcnow().date()
             for vndb_id in all_visual_novels:
                 rating, popularity, vote_count = vndb_socket_update_vn(sock, vndb_id)
                 print(vndb_id, rating, popularity, vote_count)
-                VisualNovel.objects.filter(vndb_id=vndb_id).update(
+                vn = VisualNovel.objects.filter(vndb_id=vndb_id)
+                vn.update(
                     rate=rating,
                     popularity=popularity,
                     vote_count=vote_count
                 )
+                try:
+                    VisualNovelStats.objects.get(visual_novel=vn, data=now_date)
+                except VisualNovelStats.DoesNotExist:
+                    VisualNovelStats.objects.create(visual_novel=vn, rate=rating, popularity=popularity,
+                                                    vote_count=vote_count)
         finally:
             vndb_socket_logout(sock)
         pass
