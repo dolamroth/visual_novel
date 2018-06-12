ebf35f96722216162c0d4c7a43b141f30cbfdad0
SergeySayamov <s.sayamov@logistictech.ru>
2018-06-12 19:56:34 +0300

    Tooltips on chart main page

--- visual_novel/chart/views.py
+++ visual_novel/chart/views.py
@@ -129,6 +129,7 @@ def chart_index_page(
         vn_context['title'] = visual_novel.title
         vn_context['poster_url'] = settings.POSTER_STOPPER_URL if not visual_novel.photo else visual_novel.photo.url
         vn_context['description'] = visual_novel.description
+        vn_context['alias'] = visual_novel.alias
         vn_context['genres'] = list()
         vn_context['vndb_id'] = visual_novel.vndb_id
         vn_context['chart_link'] = os.path.join('/chart/', visual_novel.alias)
@@ -138,14 +139,20 @@ def chart_index_page(
         for genre in visual_novel.vngenre_set.all().order_by('-weight'):
             vn_context['genres'].append({
                 'title': genre.genre.title,
-                'link': os.path.join('/chart/', 'genre', genre.genre.alias)
+                'link': os.path.join('/chart/', 'genre', genre.genre.alias),
+                'description': genre.genre.description,
+                'has_description': not not genre.genre.description,
+                'alias': genre.genre.alias
             })
 
         vn_context['studios'] = list()
         for studio in visual_novel.vnstudio_set.all().order_by('-weight'):
             vn_context['studios'].append({
                 'title': studio.studio.title,
-                'link': os.path.join('/chart/', 'studio', studio.studio.alias)
+                'link': os.path.join('/chart/', 'studio', studio.studio.alias),
+                'description': studio.studio.description,
+                'has_description': not not studio.studio.description,
+                'alias': studio.studio.alias
             })
 
         row.append(vn_context)
