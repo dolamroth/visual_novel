bfbceb1b37b9b2105307de588b8f9b2c3cd09cc5
Sergey Sayamov <dolamroth@mail.ru>
2018-06-19 23:42:05 +0300

    Send statistics to VK group (#66)

--- visual_novel/translation/urls.py
+++ visual_novel/translation/urls.py
@@ -1,7 +1,6 @@
-from django.urls import path, include
+from django.urls import path
 
 from . import views as translation_views
-from .api import urls as api_urls
 
 urlpatterns = [
     path(r'<str:vn_alias>/edit', translation_views.edit_statistics, name='statistics_edit'),
