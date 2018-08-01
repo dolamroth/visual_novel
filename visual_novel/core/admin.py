857599b6acee6461889e5b28142587fa358d71a2
SergeySayamov <s.sayamov@logistictech.ru>
2018-08-01 10:10:18 +0300

    Fix admin filter for Profile model by weekdays field

--- visual_novel/core/admin.py
+++ visual_novel/core/admin.py
@@ -1,6 +1,5 @@
 from bitfield import BitField
 from bitfield.forms import BitFieldCheckboxSelectMultiple
-from bitfield.admin import BitFieldListFilter
 
 from django.contrib import admin
 
@@ -14,19 +13,36 @@ class TranslationSubscriptionInline(admin.TabularInline):
     extra = 3
 
 
+class WeekdaysFilter(admin.SimpleListFilter):
+    title = 'Рассылки по дням недели'
+    parameter_name = 'weekdays'
+
+    def lookups(self, request, model_admin):
+        return_tuple = []
+        weekdays_bitfield = [d for d in Profile._meta.fields if d.name == 'weekdays'][0]
+        labels = weekdays_bitfield.labels
+        k = 1
+        for i in range(0, len(labels)):
+            return_tuple.append((k, labels[i]))
+            k *= 2
+        return return_tuple
+
+    def queryset(self, request, queryset):
+        if self.value():
+            return queryset.filter(weekdays=self.value())
+        return queryset
+
+
 class ProfileAdmin(admin.ModelAdmin):
     inlines = (TranslationSubscriptionInline,)
     list_display = (
         '__str__', 'is_staff', 'is_superuser', 'email_confirmed'
     )
     list_filter = (
-        'user__is_staff', ('weekdays', BitFieldListFilter,)
+        'user__is_staff', WeekdaysFilter
     )
     formfield_overrides = {
         BitField: {'widget': BitFieldCheckboxSelectMultiple},
     }
-
-
-
 
 admin.site.register(Profile, ProfileAdmin)
