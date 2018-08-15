014f4a962de2089fcb6d571f2330bb4212d6b065
SergeySayamov <s.sayamov@logistictech.ru>
2018-08-15 21:26:54 +0300

    List 3 most visited genres, tags etc on chart main page

--- visual_novel/vn_core/management/commands/update_ranks_by_visits.py
+++ visual_novel/vn_core/management/commands/update_ranks_by_visits.py
@@ -1,17 +1,21 @@
+import datetime
+
 from django.core.management.base import BaseCommand
 
 from cinfo.models import Genre, Tag, Studio, Staff
+from vn_core.models import VisualNovelStats, VisualNovel
 
 from vn_core.utils import YandexMetrica
 
 
 class Command(BaseCommand):
+    help = 'Update statistics for first 3 positions by page visits, for chart pages filtered by tags, genres etc.'
+
     def handle(self, *args, **options):
         metrika = YandexMetrica()
 
         Genre.objects.filter().update(rank_by_visits=None)
         top_genres = metrika.get_genres_by_popularity()
-        print(top_genres)
         if len(top_genres) >= 3:
             Genre.objects.filter(alias=top_genres[0]).update(rank_by_visits=1)
             Genre.objects.filter(alias=top_genres[1]).update(rank_by_visits=2)
@@ -37,3 +41,18 @@ class Command(BaseCommand):
             Staff.objects.filter(alias=top_staff[0]).update(rank_by_visits=1)
             Staff.objects.filter(alias=top_staff[1]).update(rank_by_visits=2)
             Staff.objects.filter(alias=top_staff[2]).update(rank_by_visits=3)
+
+        today = datetime.date.today()
+        if VisualNovelStats.objects.filter(date=today).exists():
+            # Prevent from second run
+            if not VisualNovelStats.objects.filter(date=today, rank_by_visits__isnull=False).exists():
+                top_visual_novels = metrika.get_novels_by_popularity()
+                k = 1
+                for alias in top_visual_novels:
+                    # metrika.get_novels_by_popularity has inner check that all aliases
+                    # correspond to VisualNovel objects in the database
+                    visual_novel = VisualNovel.objects.get(alias=alias)
+                    today_vn_stats = VisualNovelStats.objects.get(visual_novel=visual_novel, date=today)
+                    today_vn_stats.rank_by_visits = k
+                    today_vn_stats.save()
+                    k += 1
