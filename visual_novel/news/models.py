f64e3b72c2a01e9046b926c41bb4bf4da400e28e
Sergey Sayamov <dolamroth@mail.ru>
2023-12-15 19:43:41 +0300

    Убрал SanitizedTextField

--- visual_novel/news/models.py
+++ visual_novel/news/models.py
@@ -2,8 +2,6 @@ from django.db import models
 from django.conf import settings
 from django.contrib.auth.models import User
 
-from sanitizer.models import SanitizedTextField
-
 from core.models import PublishFileModel
 from core.fields import ImageFieldWithEnhancedUploadTo
 
@@ -12,13 +10,8 @@ class News(PublishFileModel):
     title = models.CharField(verbose_name='Заголовок', max_length=255)
     alias = models.CharField(verbose_name='Алиас (до 30 символов)', max_length=30, default='', blank=True)
     poster = ImageFieldWithEnhancedUploadTo(verbose_name='Постер', null=True, blank=True)
-    short_description = SanitizedTextField(verbose_name='Краткое описание', max_length=512, default='', blank=True,
-        strip=False, allowed_attributes=['href', 'src', 'style', 'title'],
-        allowed_tags=['a', 'p', 'img', 'table', 'tr', 'td', 'th', 'tbody', 'thead', 'span', 'div', 'br', 'b', 'i'])
-    description = SanitizedTextField(max_length=6000,
-        allowed_tags=['a', 'p', 'img', 'table', 'tr', 'td', 'th', 'tbody', 'thead', 'span', 'div', 'br', 'b', 'i'],
-        allowed_attributes=['href', 'src', 'style', 'title'],
-        default='', blank=True, strip=False, verbose_name='Описание')
+    short_description = models.TextField(verbose_name='Краткое описание', default='', blank=True)
+    description = models.TextField(default='', blank=True, verbose_name='Описание')
     created_at = models.DateTimeField(verbose_name='Время создания новости', auto_now_add=True)
     author = models.ForeignKey(User, blank=True, null=True, verbose_name="Автор", on_delete=models.SET_NULL)
 
@@ -35,15 +28,3 @@ class News(PublishFileModel):
 
     def __str__(self):
         return self.title
-
-
-News._meta.get_field('short_description').help_text = "Допустимо использовать html-теги {} и парамеры {}".format(
-        ', '.join(['<b>' + d + '</b>' for d in News._meta.get_field('short_description')._sanitizer_allowed_tags]),
-        ', '.join(['<b>' + d + '</b>' for d in News._meta.get_field('short_description')._sanitizer_allowed_attributes])
-    )
-
-News._meta.get_field('description').help_text = "Допустимо использовать html-теги {} и парамеры {}".format(
-        ', '.join(['<b>' + d + '</b>' for d in News._meta.get_field('description')._sanitizer_allowed_tags]),
-        ', '.join(['<b>' + d + '</b>' for d in News._meta.get_field('description')._sanitizer_allowed_attributes])
-    )
-
