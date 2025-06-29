dfb75efb03a53fc0816097004f4066849d799887
Sergey Sayamov <dolamroth@mail.ru>
2025-06-29 17:39:32 +0300

    Update update_votes.py

--- visual_novel/vn_core/management/commands/update_votes.py
+++ visual_novel/vn_core/management/commands/update_votes.py
@@ -10,9 +10,6 @@ from vn_core.utils import VndbStats
 from notifications.vk import VK
 
 
-cache = caches["default"]
-
-
 class Command(BaseCommand):
     def handle(self, *args, **options):
         vndb = VndbStats()
@@ -27,36 +24,28 @@ class Command(BaseCommand):
             return
 
         try:
-            seen_vndb_id = set()
-            all_visual_novels = VisualNovel.objects.all().values_list('vndb_id', flat=True)
             today = datetime.date.today()
+            sq = VisualNovelStats.objects.filter(visual_novel_id=OuterRef("id"), date=today).values_list("id")
+            all_visual_novels = VisualNovel.objects.filter(~Exists(sq)).values_list('vndb_id', flat=True)
 
-            for vndb_id in all_visual_novels:
-                if vndb_id in seen_vndb_id:
-                    continue
+            for vndb_id in set(all_visual_novels):
+                for visual_novel in VisualNovel.objects.filter(vndb_id=vndb_id):
+                    try:
+                        stats = VisualNovelStats.objects.get(visual_novel=visual_novel, date=today)
+                    except:
+                        rating, popularity, vote_count = vndb.update_vn(vndb_id)
 
-                seen_vndb_id.add(vndb_id)
-                today = datetime.date.today()
-                sq = VisualNovelStats.objects.filter(visual_novel_id=OuterRef("id"), date=today).values_list("id")
-                all_visual_novels = VisualNovel.objects.filter(~Exists(sq)).values_list('vndb_id', flat=True)
+                        stats, created = VisualNovelStats.objects.get_or_create(visual_novel=visual_novel, date=today)
 
-                for visual_novel in visual_novels:
-                    stats, created = VisualNovelStats.objects.get_or_create(visual_novel=visual_novel, date=today)
+                        visual_novel.rate = rating
+                        visual_novel.popularity = popularity
+                        visual_novel.vote_count = vote_count
+                        visual_novel.save(update_fields=["rate", "popularity", "vote_count"])
 
-                    if created:
-                        continue
-
-                    rating, popularity, vote_count = vndb.update_vn(vndb_id)
-
-                    visual_novel.rate = rating
-                    visual_novel.popularity = popularity
-                    visual_novel.vote_count = vote_count
-                    visual_novel.save(update_fields=["rate", "popularity", "vote_count"])
-
-                    stats.rate = rating
-                    stats.popularity = popularity
-                    stats.vote_count = vote_count
-                    stats.save(update_fields=["rate", "popularity", "vote_count"])
-                    time.sleep(5)
+                        stats.rate = rating
+                        stats.popularity = popularity
+                        stats.vote_count = vote_count
+                        stats.save(update_fields=["rate", "popularity", "vote_count"])
+                        time.sleep(5)
         finally:
             vndb.logout()
